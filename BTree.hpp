67ce774359aa8a2b5e205db7c1f7644d306bc531
lac518020910179 <51500362+lac518020910179@users.noreply.github.com>
2019-06-11 01:48:39 +0800

    Update BTree.hpp

--- BTree.hpp
+++ BTree.hpp
@@ -22,7 +22,7 @@ class BTree {
         ~FileName(if(str!=NULL)delete str;)
         void setname(int id)
         {
-            //if(id<0||id>9)
+            if(id<0||id>9)throw "no more B plus Tree!";
             str[0]='d';
             str[1]='a';
             str[2]='t';
@@ -79,7 +79,7 @@ class BTree {
     {
         ssize_t offset;
         ssize_t father;
-        ssize_t children[M+1];
+        ssize_t child[M+1];
         Keyt key[M+1];
         int num;//num o key
         bool leafornot;//0-noop 1-yeph child is leaf or not
@@ -87,7 +87,7 @@ class BTree {
         {
             offset=0;
             father=0;
-            for(int i=0;i<M+1;i++)children[i]=0;
+            for(int i=0;i<M+1;i++)child[i]=0;
             num=0;
             leafornot=0;
         }
@@ -218,14 +218,14 @@ class BTree {
             int pos=0;
             for(;pos<tem.num&&key>=tem.key[pos];pos++);
             if(pos==0)return 0;
-            return tem.children[pos-1];
+            return tem.child[pos-1];
         }
         else
         {
             int pos=0;
             for(;pos<tem.num&&key>=tem.key[pos];pos++);
             if(pos==0)return 0;
-            return leaflocate(key,tem.children[pos-1]);
+            return leaflocate(key,tem.child[pos-1]);
         }
     }
 
@@ -249,12 +249,166 @@ class BTree {
         return std::pair<iterator,OperationResult>(iterator(),Success);
     }
 
+    void insert_node(internalnode &internal,const Keyt &key,const ssize_t child)
+    {
+        int pos=1;
+        for(;pos<=internal.num&&key>=internal.key[pos];pos++);
+        for(int i=internal.num;i>=pos;i--){internal.key[i+1]=internal.key[i];internal.child[i+1]=inter.child[i];}        
+        internal.key[pos]=key;
+        internal.child[pos]=child;
+        internal.num++;
+        if(internal.num<M-1)write(&internal,internal.offset,1,sizeof(internalnode));
+        else split_node(internal);
+        
+    }
+    
+    void split_leaf(leafnode &leaf,const Key &key)
+    {
+        leafnode another;
+        another.num=leaf.num-(leaf.num/2);
+        leaf.num/=2;
+        another.offset=info.eof;
+        another.offset=info.eof;
+        another.father=info.father;
+        another.next=leaf.next;
+        another.pre=leaf.offset;
+        leaf.next=another.offset;
+        info.eof+=sizeof(leafnode);
+        for(int i=0;i<another.num;i++)
+        {
+            another.data[i].first=leaf.data[i+leaf.num].first;
+            another.data[i].second=leaf.data[i+leaf.num].second;
+        }
+        if(another.next==0)info.tail=another.offset;
+        else
+        {
+            leafnode next_leaf;
+            read(&next_leaf,another.next,1,sizeof(leafnode));
+            next_leaf.pre=another.offset;
+            write(&next_leaf,another.next,1,sizeof(leafnode));
+        }
+        
+        write(&info,bpt_info_offset,1,sizeof(bpt_Info));
+        write(&leaf,leaf.offset,1,sizeof(leafnode));
+        write(&another,another.offset,1,sizeof(leafnode));
+        internalnode father;
+        read(&father,leaf.father,1,sizeof(internalnode));
+        insert_node(father,another.data[0].first,another.offset);
+    }
+    
+    void split_node(internalnode &internal)
+    {
+        if(internal.offset!=info.root)
+        {
+            internalnode another;
+            another.offset=info.eof;
+            info.eof+=sizeof(internalnode);
+            another.num=internal.num-(internal.num/2)-1;
+            internal.num/=2;
+            another.father=internal.father;
+            another.type=internal.type;
+            for(int i=0;i<=another.num;i++)
+            {
+                another.child[i]=internal.child[i+1+internal.num];
+            }
+            for(int i=1;i<=another.num;i++)
+            {
+                another.key[i]=internal.key[i+1+internal.num];
+            }
+            write(&internal,internal.offset,1,sizeof(internalnode));
+            write(&another,another.offset,1,sizeof(internalnode));
+            
+            internalnode tem;
+            leafnode leaf;
+            for(int i=0;i<=another;i++)
+            {
+                if(another.type==1)
+                {
+                    read(&leaf,another.child[i],1,sizeof(leafnode));
+                    leaf.father=another.offset;
+                    write(&leaf,another.child[i],1,sizeof(leafnode));
+                }
+                else
+                {
+                    read(&tem,another.child[i],1,sizeof(internalnode));
+                    tem.father=another.offset;
+                    write(&tem,another.child[i],1,sizeof(internalnode));
+                }
+            }
+            read(&tem,internal.father,1,sizeof(internalnode));
+            insert_node(tem,internal.key[internal.nu+1],another.offset);
+            
+        }
+        else
+        {
+            internalnode new_root;
+            internalnode half;
+            new_root.offset=info.eof;
+            info.eof+=sizeof(internalnode);
+            half.offset=info.eof;
+            info.eof+=sizeof(internalnode);
+            info.root=new_root.offset;
+            internal.father=info.root;
+            half.father=info.root;
+            half.num=internal.num-(internal.num/2)-1;
+            for(int i=0;i<=half.num;i++)
+            {
+                half.child[i]=internal.child[i+internal.num/2+1];
+            }
+            for(int i=1;i<=half.num;i++)
+            {
+                half.key[i]=internal.key[i+internal.num/2+1];
+            }
+            new_root.num=1;
+            new_root.father=0;
+            new_root.t=0;
+            half.type=internal.type;
+            new_root.child[0]=internal.offset;
+            new_root.child[1]=half.offset;
+            new_root.key[1]=internal.key[internal.num/2+1];
+            internal.num/=2;
+            if(internal.type)
+            {
+                leafnode change;
+                for(int i=0;i<=half.num;i++)
+                {
+                    read(&change,internal.child[i+internal.num+1],1,sizeof(leafnode));
+                    change.father=half.offset;
+                    write(&change,internal.child[i+internal.num+1],1,sizeof(leafnode));
+                }
+            }
+            else
+            {
+                internalnode change;
+                for(int i=0;i<=half.num;i++)
+                {
+                    read(&change,internal.child[i+internal.num+1],1,sizeof(internalnode));
+                    change.father=half.offset;
+                    write(&change,internal.child[i+internal.num+1],1,sizeof(internalnode));
+                }
+            }
+            write(&info,bpt_info_offset,1,sizeof(bpt_Info));
+            write(&internal,internal.offset,1,sizeof(internalnode));
+            write(&new_root,new_root.offset,1,sizeof(internalnode));
+            write(&half,half.offset,1,sizeof(internalnode));
+            return;
+        }
+        
+    }
 
  public:
-
+    class iterator;
+    class const_iterator;
+    
 
   // Default Constructor and Copy Constructor
   BTree() {
+      fp_name.setname(ID);
+      fp=nullptr;
+      fp_open=false;
+      openfile();
+      initialbuild();
+      
     // Todo Default
   }
   BTree(const BTree& other) {
@@ -264,12 +418,25 @@ class BTree {
     // Todo Assignment
   }
   ~BTree() {
+      closefile();
     // Todo Destructor
   }
   // Insert: Insert certain Key-Value into the database
   // Return a pair, the first of the pair is the iterator point to the new
   // element, the second of the pair is Success if it is successfully inserted
   pair<iterator, OperationResult> insert(const Key& key, const Value& value) {
+      open();
+      ssize_t offset=leaflocate(key,info.root);
+      leafnode leaf;
+      read(&leaf,offset,1,sizeof(leafnode));
+      std::pair<iterator, OperationResult> tem;
+      tem=insert_leaf(leaf,key,value);
+      pair<iterator,OperationResult> result;
+      result.first=tem.first;
+      result.second=tem.second;
+      return result;
+      
+      
     // TODO insert function
   }
   // Erase: Erase the Key-Value
@@ -277,27 +444,27 @@ class BTree {
   // Return Fail if the key doesn't exist in the database
   OperationResult erase(const Key& key) {
     // TODO erase function
-    return Fail;  // If you can't finish erase part, just remaining here.
+    return Success;  // If you can't finish erase part, just remaining here.
   }
   // Overloaded of []
   // Access Specified Element
   // return a reference to the first value that is mapped to a key equivalent to
   // key. Perform an insertion if such key does not exist.
-  Value& operator[](const Key& key) {}
+  //Value& operator[](const Key& key) {}
   // Overloaded of const []
   // Access Specified Element
   // return a reference to the first value that is mapped to a key equivalent to
   // key. Throw an exception if the key does not exist.
-  const Value& operator[](const Key& key) const {}
+  //const Value& operator[](const Key& key) const {}
   // Access Specified Element
   // return a reference to the first value that is mapped to a key equivalent to
   // key. Throw an exception if the key does not exist
-  Value& at(const Key& key) {}
+  //Value& at(const Key& key) {}
   // Overloaded of const []
   // Access Specified Element
   // return a reference to the first value that is mapped to a key equivalent to
   // key. Throw an exception if the key does not exist.
-  const Value& at(const Key& key) const {}
+  //const Value& at(const Key& key) const {}
   // Return a iterator to the beginning
   iterator begin() {return iterator();}
   const_iterator cbegin() const {return const_iterator();}
@@ -305,17 +472,22 @@ class BTree {
   iterator end() {return iterator();}
   const_iterator cend() const {return const_iterator();}
   // Check whether this BTree is empty
-  bool empty() const {}
+  bool empty() const {return info.size==0;}
   // Return the number of <K,V> pairs
-  size_t size() const {}
+  size_t size() const {return info.size;}
   // Clear the BTree
-  void clear() {}
+  void clear() 
+  {
+      fp=fopen(fp_name.str,"w");
+      fclose(fp);
+      initialbuild();
+  }
   /**
    * Returns the number of elements with key
    *   that compares equivalent to the specified argument,
    * The default method of check the equivalence is !(a < b || b > a)
    */
-  size_t count(const Key& key) const {}
+  //size_t count(const Key& key) const {}
   /**
    * Finds an element with key equivalent to key.
    * key value of the element to search for.
